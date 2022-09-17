c0a438651fac862a04357c36ac5f506c0dcb06af
Debasish Dutta <ddmasterdon@gmail.com>
2022-09-18 01:14:49 +0530

    added initial pages

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// window.addEventListener("scroll", ()=> {
+//     let scrollTop = document.documentElement.scrollTop;        
+//     let scrollDown = document.documentElement.scrollDown;
+//     document.querySelector('.intro').style.width = 100 + scrollTop / 5 + '%';
+//     document.querySelector('.intro').style.width = 100 - scrollDown / 5 + '%';
+    
+// });

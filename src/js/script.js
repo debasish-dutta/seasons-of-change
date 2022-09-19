e9d664d7b1b76cc159e13bcd2a2db9198a412f2d
Debasish Dutta <ddmasterdon@gmail.com>
2022-09-20 02:07:51 +0530

    made changed to button

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener("scroll", ()=> {
-//     let scrollTop = document.documentElement.scrollTop;        
-//     let scrollDown = document.documentElement.scrollDown;
-//     document.querySelector('.intro').style.width = 100 + scrollTop / 5 + '%';
-//     document.querySelector('.intro').style.width = 100 - scrollDown / 5 + '%';
-    
-// });
